8d7503cba36a18a56a1ea23c30c5630d9e4e1588
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-25 19:49:02 +0800

    imported index.css to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import {
   Route
 } from "react-router-dom";
 
+import './index.css'
+
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
