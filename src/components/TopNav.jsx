ce0bee8d811752ce31af681c3173b9d090f3da4e
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-27 14:33:52 +0800

    centered the nav_ul using margin auto

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,11 +4,11 @@ export default function TopNav() {
   return (
     <nav className='flex'>
       <div className='inline-block p-3'>
-        <FaBars size="2em "/>
+        <FaBars size="2em"/>
       </div>
-      <ul className='nav__ul'>
-        <li className='inline-block p-3'>Home</li>
-        <li className='inline-block p-3'>About</li>
+      <ul className='nav__ul h-fit'>
+        <li className='inline-block'>Home</li>
+        <li className='inline-block'>About</li>
       </ul>
     </nav>
   )
