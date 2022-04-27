cc3a4a1242f7a95e6ee84c881170e4854336495c
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-27 12:51:52 +0800

    added basic topnav with design

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { FaBars } from 'react-icons/fa';
 export default function TopNav() {
   return (
-    <div>TopNav</div>
+    <nav className='flex'>
+      <div className='inline-block p-3'>
+        <FaBars size="2em "/>
+      </div>
+      <ul className='nav__ul'>
+        <li className='inline-block p-3'>Home</li>
+        <li className='inline-block p-3'>About</li>
+      </ul>
+    </nav>
   )
 }
