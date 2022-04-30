21ae50b6e44b00d371da6da4a8305d7e2e86b68a
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-30 15:15:16 +0800

    added some content to about me page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,19 @@
 export default function About() {
   return (
-    <div>About</div>
+    <section className="h-screen pt-16 px-9 grid place-content-center place-items-center">
+        <h2 className="p-4">[About me]</h2>
+
+        <section className="max-w-lg">
+          <p className="text-justify mb-5">
+            Hi, my name is <span className="text-emerald-500">Hinrich</span>.<br/>
+            I am currently a 3rd year undergrad studying at AMA Computer College  and an intern at 
+            <a href="http://poblacion.ph/" className="text-emerald-500"> Poblacion.ph</a> which is a startup Ecommerce company.
+          </p>
+
+          <p className="text-justify mb-5">
+            I started learning how to code when I was about to go to college. After my first year, I decided to take web development as my route. Since then, I have been learning its ecosystem.
+          </p>
+        </section>
+      </section>
   )
 }
