fa18beb311ee9a86317d2e0203421de97d3a518a
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-30 15:16:35 +0800

    added simple landing page text

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      Home
+      <section className="grid place-items-center h-screen px-10">
+        <h2 className="text-center">
+          Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
+        </h2>
+      </section>
     </div>
   )
 }
