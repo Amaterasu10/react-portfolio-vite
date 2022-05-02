ac2fae6947c1651b83c398b66607d8f51ba40364
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-02 15:07:17 +0800

    corrected the path for the resume and configured to open it in a new tab instead of downloading it

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
           <h2 className="text-center">
             Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
           </h2>
-          <a href="./assets/files/resume.pdf" download className="bg-slate-100 hover:bg-slate-200 px-7 py-1 border-2 border-solid border-black">Resume</a>
+          <a href="../src/assets/files/resume.pdf" target="_blank"  className="bg-slate-100 hover:bg-slate-200 px-7 py-1 border-2 border-solid border-black">Resume</a>
         </div>
       </section>
     </div>
