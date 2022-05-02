45c2316b2bd4729cddb3dd1a49a807b7f0159a33
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-02 13:40:52 +0800

    added resume button

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,9 +2,12 @@ export default function Home() {
   return (
     <div>
       <section className="grid place-items-center h-screen px-10">
-        <h2 className="text-center">
-          Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
-        </h2>
+        <div className="grid place-items-center gap-y-5">
+          <h2 className="text-center">
+            Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
+          </h2>
+          <button className="bg-slate-100 hover:bg-slate-200 px-7 py-1 border-2 border-solid border-black">Resume</button>
+        </div>
       </section>
     </div>
   )
