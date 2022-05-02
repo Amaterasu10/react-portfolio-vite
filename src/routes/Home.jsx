0bd5cac8f92ed8d4184de5d966aa10cf7049d430
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-02 15:57:29 +0800

    commented out the resume for testing

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import resume from "../assets/files/resume.pdf"
+// import resume from "../assets/files/resume.pdf"
 export default function Home() {
   return (
     <div>
@@ -7,7 +7,7 @@ export default function Home() {
           <h2 className="text-center">
             Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
           </h2>
-          <a href={resume} target="_blank"  className="bg-slate-100 hover:bg-slate-200 px-7 py-1 border-2 border-solid border-black">Resume</a>
+          {/* <a href={resume} target="_blank"  className="bg-slate-100 hover:bg-slate-200 px-7 py-1 border-2 border-solid border-black">Resume</a> */}
         </div>
       </section>
     </div>
