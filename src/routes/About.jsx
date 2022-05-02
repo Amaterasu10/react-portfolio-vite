566bcc68c69dec6e9cd857a2c8c6816922952c3e
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-02 19:27:56 +0800

    added icons for the techstack

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,3 +1,13 @@
+import { 
+  SiHtml5, 
+  SiCss3,
+  SiSass, 
+  SiTailwindcss, 
+  SiJavascript, 
+  SiReact, 
+  SiTypescript, 
+  SiVite 
+} from 'react-icons/si';
 export default function About() {
   return (
     <div>
@@ -18,8 +28,46 @@ export default function About() {
 
       <section className="h-screen px-9 grid place-content-center place-items-center">
         <h2 className="p-4">[Tech stack]</h2>
-        <section className="max-w-lg bg-slate-200 p-3 shadow-inner rounded-lg">
-          Html, CSS, Sass, Tailwind, JavaScript, React.js, Typscript, Vite
+        <section className="w-fit max-w-lg bg-slate-200 p-5 shadow-inner rounded-lg flex justify-around flex-wrap">
+          
+          <div className='p-2'>
+            <SiHtml5 title='Html5' className='text-xl md:text-4xl text-red-600'/>
+          </div>
+
+          <div className="p-2">
+          <SiCss3 title='CSS3' className='text-xl md:text-4xl text-blue-600'/> 
+          </div>
+
+          <div className="p-2">
+          <SiSass title='Sass' className='text-xl md:text-4xl text-pink-600'/> 
+          </div>
+
+          <div className="p-2">
+          <SiTailwindcss title='TailwindCss' className='text-xl md:text-4xl text-teal-500'/> 
+          </div>
+
+          <div className="p-2">
+            <div className='bg-black'>
+              <SiJavascript title='JavaScript' className='text-xl md:text-4xl text-yellow-500'/> 
+            </div>
+          </div>
+
+          <section className=' mt-5 w-full rounded-lg bg-slate-300' title='still learning'>
+            <h3 className='text-center'>currently learning</h3>
+            <div className='flex justify-around flex-wrap '>
+              <div className="p-2">
+                <SiReact title='React.js' className='text-xl md:text-4xl text-cyan-500'/> 
+              </div>
+
+              <div className="p-2">
+                <SiTypescript title='Typscript' className='text-xl md:text-4xl text-sky-700'/> 
+              </div>
+              
+              <div className="p-2">
+                <SiVite title='Vite' className='text-xl md:text-4xl text-violet-600'/>
+              </div>
+            </div>
+          </section>
         </section>
       </section>
     </div>
