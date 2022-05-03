953a6ab709133df6a8592db5506a4a8c25ce7af3
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-03 16:23:37 +0800

    set the alignment of the texts to center

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -6,12 +6,12 @@ export default function About() {
         <h2 className="p-4">[About me]</h2>
 
         <section className="max-w-lg">
-          <p className="text-justify mb-5">
+          <p className="text-center mb-5">
             Hi, my name is <span className="text-emerald-500">Hinrich</span>.<br/>
             I am currently a 3rd year undergrad studying at AMA Computer College  and an intern at <a href="http://poblacion.ph/" className="text-emerald-500 underline">Poblacion.ph</a> which is a startup eCommerce company.
           </p>
 
-          <p className="text-justify mb-5">
+          <p className="text-center mb-5">
             I started learning how to code when I was about to go to college. After my first year, I decided to take web development as my route. Since then, I have been learning its ecosystem.
           </p>
         </section>
