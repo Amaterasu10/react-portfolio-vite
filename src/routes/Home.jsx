43ba69502277a6cdc9dd0509f7e0ef5a239ff82c
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-03 15:29:23 +0800

    replaced div element with an empty fragment to reduce divs

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,7 @@
 import resume from "../assets/files/resume.pdf"
 export default function Home() {
   return (
-    <div>
+    <>
       <section className="grid place-items-center h-screen px-10">
         <div className="grid place-items-center gap-y-5">
           <h2 className="text-center">
@@ -10,6 +10,6 @@ export default function Home() {
           <a href={resume} target="_blank"  className="button-link">Resume</a>
         </div>
       </section>
-    </div>
+    </>
   )
 }
