d70fb3a7fb90b180af7bb08de9ae334d21448722
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-03 16:37:11 +0800

    changed the landing page text so the name is more prominent

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,10 @@ export default function Home() {
       <section className="grid place-items-center h-screen px-10">
         <div className="grid place-items-center gap-y-5">
           <h2 className="text-center">
-            Hi, my name is <span className="text-emerald-500">Hinrich</span> and welcome to my simple <span className="text-emerald-500">portfolio</span>
+          <span className="text-xl font-bold">
+            Hi, I'm <span className="text-emerald-600">Hinrich</span>
+          </span>.<br/> 
+          welcome to my simple <span className="text-emerald-600">portfolio</span>
           </h2>
           <a href={resume} target="_blank"  className="button-link">Resume</a>
         </div>
