00777c9dea3c5f03936d653747da774efe5b9fe1
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-05 14:43:53 +0800

    replaced the default shadow-md and shadow-lg for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
       fontFamily: {
         "poppins": ['Poppins', 'sans-serif'],
         "poppins-semibold": ['Poppins-semibold', 'sans-serif']
+      },
+      boxShadow: {
+        "md": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+        "lg": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
       }
     },
   },
