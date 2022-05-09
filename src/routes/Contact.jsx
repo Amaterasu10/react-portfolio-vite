90a4c3c9b43888ce241374e97602c824a11e56dd
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-09 16:59:52 +0800

    adjusted the padding and margin for the contact form

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className="min-h-screen h-fit grid-center pt-12">
-      <div className='w-full max-w-md my-5'>
+    <div className="min-h-screen h-fit grid-center pt-12 p-5">
+      <div className='w-full max-w-md mt-5'>
         <form 
         name="contact" 
         method="POST" 
