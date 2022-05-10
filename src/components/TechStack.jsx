70c6c75ba650ab2a64f1173963a8c98bbf5afb8e
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-10 23:22:10 +0800

    removed the styles for techstack that causes it to look boxy

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -14,8 +14,8 @@ import vite from '../favicon.svg'
 export default function TechStack() {
   return (
     <>
-        <h2 className="p-4">[Tech stack]</h2>
-        <section className="w-fit max-w-lg bg-slate-200 p-5 shadow-inner rounded-lg flex justify-around flex-wrap">
+        <h2 className="p-4 text-4xl text-center">Tech Stack</h2>
+        <section className="w-fit max-w-lg p-5 flex justify-around flex-wrap">
             
             <div className='p-2 min-w-[89px]'>
                 <SiHtml5 title='Html5' className='icon text-red-600' /> 
@@ -44,8 +44,8 @@ export default function TechStack() {
                 <p className='text-center'>JavaScript</p>
             </div>
 
-            <section className='p-4 mt-5 w-full rounded-lg bg-slate-300' title='still learning'>
-                <h3 className='text-center'>[currently learning]</h3>
+            <section className='p-4 mt-5 w-full'>
+                <h3 className='text-3xl text-center'>Learning</h3>
                 <div className='flex justify-around flex-wrap mt-2'>
                     <div className="p-2 min-w-[89px]">
                     <SiReact title='React.js' className='icon text-cyan-500' /> 
