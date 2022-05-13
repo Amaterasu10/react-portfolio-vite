76f1d37e01ef283a3c481c6c2639cb76ce9af1c8
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-13 22:06:35 +0800

    replaced boxShadow with dropShadow and commented it out first

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,10 @@ module.exports = {
         "poppins": ['Poppins', 'sans-serif'],
         "poppins-semibold": ['Poppins-semibold', 'sans-serif']
       },
-      boxShadow: {
-        "md": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
-        "lg": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-      }
+      // dropShadow: {
+      //   "md": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+      //   "lg": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
+      // }
     },
   },
   plugins: [],
