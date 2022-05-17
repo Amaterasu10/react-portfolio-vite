8e98f6ba599f379ad7348fac535a8cd290a9a4ce
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-17 10:38:27 +0800

    added animation to blob.webm so that it shows up from opacity 0then gradually becomes visible

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,6 +2,13 @@ import TechStack from '../components/TechStack';
 import blobWebm from "../assets/images/blob.webm";
 import blobMp4 from "../assets/images/blob.mp4";
 export default function About() {
+  const blob = useRef(null);
+  const tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 2 }});
+  useEffect(()=> {
+    
+    gsap.fromTo(blob.current, {opacity: 0}, {opacity: 1, duration: 5});
+    
+  },[])
   return (
     <>
       <section className='min-h-screen h-fit flex items-center pt-5 bg-no-repeat bg-contain bg-right relative'>
@@ -9,6 +16,7 @@ export default function About() {
           <source src={blobWebm} type="video/webm"/>
           <source src={blobMp4} type="video/mp4"/>
         </video>
+        <div className='absolute z-0 left-0 right-0 top-0 bottom-0 '></div>
 
         <section className="max-w-3xl h-fit p-5 sm:pl-24 z-[1]">
           <h2 className='pb-2 text-center sm:text-left text-4xl md:text-6xl primary-text'>
