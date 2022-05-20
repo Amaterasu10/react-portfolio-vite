4a99f03e06f94ef1bb990c899c9b450f12118f28
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-20 15:23:48 +0800

    removed the unused tailwind classNames in the about.jsx and replaced some with their shorthand version

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -5,12 +5,12 @@ export default function About() {
 
   return (
     <>
-      <section className='min-h-screen h-fit flex items-center pt-5 bg-no-repeat bg-contain bg-right relative'>
+      <section className='min-h-screen h-fit flex items-center pt-5 relative'>
         <video autoPlay loop muted playsInline className='absolute z-0 right-0 opacity-0 animate-[fadeIn_1.5s_2s_ease-in_forwards]'>
           <source src={blobWebm} type="video/webm"/>
           <source src={blobMp4} type="video/mp4"/>
         </video>
-        <div className='absolute z-0 left-0 right-0 top-0 bottom-0 '></div>
+        <div className='absolute inset-0'></div>
 
         <section className="max-w-3xl 2xl:max-w-7xl h-fit p-5 sm:pl-24 z-[1] animate-[fadeInLeft_1s_ease-in]">
           <h2 className='pb-2 text-center sm:text-left heading primary-text'>
