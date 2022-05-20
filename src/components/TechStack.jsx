184a89a7757cbfb1b30bbeaa49c80ea746f419da
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-20 14:02:43 +0800

    applied heading2 at techstack sub header

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -55,7 +55,7 @@ export default function TechStack() {
             </div>
 
             <section className='p-4 mt-5 w-full'>
-                <h3 className='text-3xl text-center'>Learning</h3>
+                <h3 className='heading2 text-center'>Learning</h3>
                 <div className='flex justify-around flex-wrap mt-2'>
                     <div className="p-2 min-w-[89px]">
                         <SiReact title='React.js' className='icon text-cyan-500' /> 
