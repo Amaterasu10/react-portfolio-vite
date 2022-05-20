1ecba695dbc4ecc59380ec4ba4abd5b39a09ba79
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-20 14:16:41 +0800

    applied icon-container to techstack icon containers

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -27,27 +27,27 @@ export default function TechStack() {
         <h2 onClick={date} className="p-4 heading text-center">Tech Stack</h2>
         <section className="w-fit max-w-lg 2xl:max-w-screen-xl p-5 mx-auto flex justify-around flex-wrap">
             
-            <div className='p-2 min-w-[89px]'>
+            <div className='icon-container'>
                 <SiHtml5 title='Html5' className='icon text-red-600' /> 
                 <p className='text-center'>Html</p>
             </div>
 
-            <div className="p-2 min-w-[89px]">
+            <div className="icon-container">
                 <SiCss3 title='CSS3' className='icon text-blue-600' /> 
                 <p className='text-center'>CSS</p>
             </div>
 
-            <div className="p-2 min-w-[89px]">
+            <div className="icon-container">
                 <SiSass title='Sass' className='icon text-pink-600' /> 
                 <p className='text-center'>Sass</p>
             </div>
 
-            <div className="p-2 min-w-[89px]">
+            <div className="icon-container">
                 <SiTailwindcss title='TailwindCSS' className='icon text-teal-500' /> 
                 <p className='text-center'>Tailwind</p>
             </div>
 
-            <div className="p-2 min-w-[89px]">
+            <div className="icon-container">
                 <div className='bg-black w-fit mx-auto'>
                     <SiJavascript title='JavaScript' className='icon text-yellow-500 mx-0' /> 
                 </div>
@@ -57,17 +57,17 @@ export default function TechStack() {
             <section className='p-4 mt-5 w-full'>
                 <h3 className='heading2 text-center'>Learning</h3>
                 <div className='flex justify-around flex-wrap mt-2'>
-                    <div className="p-2 min-w-[89px]">
+                    <div className="icon-container">
                         <SiReact title='React.js' className='icon text-cyan-500' /> 
                         <p className='text-center'>React.js</p>
                     </div>
 
-                    <div className="p-2 min-w-[89px]">
+                    <div className="icon-container">
                         <SiTypescript title='Typscript' className='icon text-sky-700' />
                         <p className='text-center'>Typscript</p> 
                     </div>
                     
-                    <div className="p-2 min-w-[89px]">
+                    <div className="icon-container">
                         <img src={vite} title='vite' className='icon max-w-[1.25rem] md:max-w-[2.25rem]' />
                         <p className='text-center'>Vite</p>
                     </div>
