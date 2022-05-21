138015dbdd10a1d31e2a7c3ed62312820f73dfb5
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-21 19:19:54 +0800

    changed the animation of the blob in Home.jsx to fadeIn 1sec

---
+++ src/components/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <>
+        <h2 className='primary-text heading text-center'>Projects</h2>
+        
+    </>
+  )
+}
