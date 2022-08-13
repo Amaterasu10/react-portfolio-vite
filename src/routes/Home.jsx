37d494e624446cbdc456657879df33317c0f8f4e
Hinrich <johnhinrichggalindo@gmail.com>
2022-08-13 17:20:19 +0800

    added test code for react-helmet at the homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,27 @@
 // import resume from "../assets/files/resume.pdf";
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import {Helmet} from "react-helmet";
 export default function Home() {
 
   return (
     <>
+      <Helmet>
+        <title>John Hinrich | Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Hi, my name is Hinrich, and this is my front-end portfolio website"/>
+
+        <meta name="keywords" content="HTML, CSS, JavaScript, john hinrich, hinrich, portfolio, john hinrich joseph g. galindo, front-end, frontend, developer, website, hinrich portfolio"/>
+
+        <meta name="author" content="John Hinrich Joseph G. Galindo"/>
+      </Helmet>
+
       <section 
         className="
         p-5 
         min-h-screen h-fit
         grid sm:flex place-items-center bg-blob"
       >
+
         <h1 className='hidden'>Hi, my name is Hinrich, and this is my front-end portfolio website</h1>
         <div className="w-fit p-5 sm:pl-24 z-10 animate-[fadeInTop_1s_ease-in] sm:animate-[fadeInLeft_1s_ease-in]">
           <h2 
