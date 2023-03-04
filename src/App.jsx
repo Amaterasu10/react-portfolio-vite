0de82c2e7a40f86438236322e871f97099cbcc19
Hinrich <johnhinrichggalindo@gmail.com>
2023-03-04 15:28:50 +0800

    Converted 2 spaces to tab size: 4. Converted the website from react-router based to scrollIntoView based single page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,97 @@
-import { Outlet } from 'react-router-dom';
-import TopNav from './components/TopNav';
+import { useRef } from 'react';
 
+import TopNav from './components/TopNav';
+import Home from './routes/Home';
+import Contact from './routes/Contact'
+import About from './routes/About'
+import Projects from './routes/Projects';
+
+import toast, { Toaster } from 'react-hot-toast';
 function App() {
 
-  return (
-    <div className="App font-poppins">
-      <header className='fixed w-full z-10'>
-        <TopNav/>
-      </header>
-      <main className='min-h-screen h-fit'>
-        <Outlet/>
-      </main>
-    </div>
-  )
+	const homeRef = useRef(null)
+	const aboutRef = useRef(null)
+	const contactRef = useRef(null)
+
+	const mainRef = useRef(null)
+
+	return (
+		<div className="App font-poppins relative">
+			<header className='fixed w-full z-50'>
+				<TopNav
+					navs={[
+						{
+							text: 'Home',
+							ref: homeRef	
+						},
+						{
+							text: 'About',
+							ref: aboutRef
+						},
+						{
+							text: 'Contact',
+							ref: contactRef
+						}
+						
+					]}
+				/>
+			</header>
+
+			<Toaster
+				position="bottom-left"
+				reverseOrder={false}
+			/>
+
+			<main className='min-h-screen h-fit scroll-smooth' ref={ mainRef }>
+				{/* <Outlet/> */}
+				<Home ref={ homeRef } />
+				
+				<About ref={ aboutRef } />
+
+				{/* <Projects/> */}
+
+				<Contact 
+					ref={ contactRef } 
+					handleSubmit={ handleSubmit } 
+				/>
+				
+			</main>
+		</div>
+	)
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log('test')
+		const myForm = event.target;
+		const formData = new FormData(myForm);
+		
+		const email = formData.get('email')
+		const name = formData.get('email')
+		const message = formData.get('message')
+
+		const someUndefined = (
+			email === '' ||
+			name === '' ||
+			message === ''
+		)
+		
+		if(email ==='') toast.error('no email was provided')
+
+		fetch("", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+		})
+		.then(
+			() => {
+				return toast.success('message sent')
+			}
+		)
+		.catch( 
+			error => toast.error(error) 
+		);
+	};
+
 }
 
 export default App
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,39 +1,73 @@
 import TechStack from '../components/TechStack';
 import blobWebm from "../assets/images/blob.webm";
 import blobMp4 from "../assets/images/blob.mp4";
-export default function About() {
+import { forwardRef } from 'react';
 
-  return (
-    <>
-      <section className='min-h-screen h-fit flex items-center pt-5 relative'>
-        <video autoPlay loop muted playsInline className='max-h-full min-h-full absolute z-0 right-0 opacity-0 animate-[fadeIn_1.5s_1s_ease-in_forwards]'>
-          <source src={blobWebm} type="video/webm"/>
-          <source src={blobMp4} type="video/mp4"/>
-        </video>
-        <div className='absolute inset-0'></div>
+const About = forwardRef(
+	function About(props, ref) {
 
-        <section className="max-w-3xl 2xl:max-w-7xl h-fit p-5 sm:pl-24 z-[1] animate-[fadeInTop_1s_ease-in] sm:animate-[fadeInLeft_1s_ease-in]">
-          <h2 className='pb-2 text-center sm:text-left heading primary-text'>
-            About me
-          </h2>
-          <p 
-            className='text mb-3 text-center sm:text-left pb-1 text-stone-600'
-          >
-            Hi, my name is <span className="primary-text">Hinrich</span>.<br/>
-            I am currently a 3rd year undergrad studying at AMA Computer College  and an intern at <a className="primary-text underline" href="http://poblacion.ph/">Poblacion.ph</a> which is a startup eCommerce company.
-          </p>
-
-          <p 
-            className='text mb-3 text-center sm:text-left pb-1 text-stone-600'
-          >
-            I started learning how to code when I was about to go to college. After my first year, I decided to take web development as my route. Since then, I have been learning its ecosystem.
-          </p>
-        </section>
-      </section>
-
-      <section className='min-h-screen h-fit grid place-content-center'>
-        <TechStack />
-      </section>
-    </>
-  )
-}
+		return (
+			<article ref={ref}>
+				<section className='min-h-screen h-fit flex items-center pt-5 relative'>
+					{/* <video autoPlay loop muted playsInline className='max-h-full min-h-full absolute z-0 right-0 opacity-0 animate-[fadeIn_1.5s_1s_ease-in_forwards]'>
+					<source src={blobWebm} type="video/webm"/>
+					<source src={blobMp4} type="video/mp4"/>
+					</video>
+	
+					{
+						//cover for blob video so that it's not clickable
+					}
+					<div className='absolute inset-0'></div> */}
+	
+					<article 
+						// className="
+						// 	2xl:max-w-7xl 
+						// 	h-fit 
+						// 	p-5 
+						// 	sm:pl-24 
+						// 	z-[1] 
+						// 	animate-[fadeInTop_1s_ease-in] 
+						// 	sm:animate-[fadeInLeft_1s_ease-in]"
+	
+						className="
+							mx-auto
+							max-w-6xl
+							2xl:max-w-7xl 
+							h-fit 
+							p-5 
+							z-[1] 
+							animate-[fadeInTop_0.5s_ease-in]"
+						>
+						{/* <h2 className='pb-2 text-center sm:text-left heading primary-text'> */}
+						<h2 className='p-4 text-center heading primary-text'>
+							About me
+						</h2>
+	
+						{/* <span className='text mb-3 text-center sm:text-left pb-1'> */}
+						<span className='text mb-3 pb-1 text-justify'>
+							<p>
+								Hi! my name is <span className="font-semibold italic">Hinrich</span>. I am an 
+								aspiring <span className="font-semibold italic">front-end web developer</span>.
+								I gained some practical experience through a 3-month internship 
+								with <a className='underline font-semibold italic' href="http://poblacion.ph" target='_blank'>Poblacion.ph</a>, 
+								a startup E-Commerce company. However, since I was a founding member, my presence was crucial to the company, and 
+								I ended up extending my internship for an additional 7 months. After my internship, I decided to focus on my studies. 
+								Now, I am looking for a company that would help me grow as a developer.
+							</p>
+						</span>
+					</article>
+				</section>
+	
+				<article 
+					className='
+						min-h-screen 
+						h-fit grid 
+						place-content-center'
+					>
+					<TechStack />
+				</article>
+			</article>
+		)
+	}
+)
+export default About;
