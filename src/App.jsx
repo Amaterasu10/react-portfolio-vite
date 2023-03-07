1ae8f3c2d645009d455ba0137d4ae356d7e2f3f2
Hinrich <johnhinrichggalindo@gmail.com>
2023-03-07 16:30:11 +0800

    reduced the intensity of the background color

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 	const mainRef = useRef(null)
 
 	return (
-		<div className="App font-poppins relative bg-slate-200">
+		<div className="App font-poppins relative bg-slate-100">
 			<header className='fixed w-fit z-50'>
 				<TopNav
 					navs={[
