e39eb46b4672b8e8f5cfb5d59e1997c27d6e0e37
Hinrich <johnhinrichggalindo@gmail.com>
2024-01-17 19:18:59 +0800

    added simple work in progress animtation for projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,21 +1,17 @@
 import React, { forwardRef } from 'react'
 import Project from '../components/Project'
+import { BsNutFill } from "react-icons/bs";
 
 const Projects = forwardRef(
     function Projects(props, ref) {
         return (
-            <article className='min-h-screen h-fit grid-center pt-12 p-5' ref={ref}>
-                <h2 className='p-4 text-center heading2 primary-text'>Projects</h2>
-                <div className='tabs-container'>
-                    <ul>
-                        <li>Javascript</li>
-                        <li>React</li>
-                    </ul>
-                    
-                    <Project title={'title'} description={'description'} images={'images'} labels={'labels'} />
-                </div>
+            <article className='min-h-screen h-fit grid-center pt-12 p-5 content-center' ref={ref}>
+               
+              <h2 className='p-4 text-center heading2 primary-text'>Projects</h2>
 
-
+              <div className='flex items-center'>
+                <h2 className='heading3'>Work in progress </h2><BsNutFill  className='animate-spin' fontSize={"2rem"}/>
+              </div>
             </article>
         )
     }
