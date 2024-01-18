3b5088c858257994fbebe79d8fcfcd9989076abd
Hinrich <johnhinrichggalindo@gmail.com>
2024-01-18 18:43:37 +0800

    added basic implementation of Project componenent

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Projects() {
+export default function Project({title, thumbnail, description }) {
   return (
-    <article>
-      <h3 className='primary-text text-center'>Projects</h3>
+    <article className='bg-slate-50 m-2 p-2 shadow-md flex flex-col lg:flex-row mt-5 rounded-md'>
+      <img src={thumbnail} alt="Thumbnail" className='p-3'/>
+      
+      <section>
+        <h3 className='primary-text heading3'>{title}</h3>
+        <p className='p-3'>
+          {description}
+        </p>
+      </section>
+      
     </article>
   )
 }
