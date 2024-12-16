18661d5d3cbc8a56313a45deb1f5b48113218cdf
Hinrich <johnhinrichggalindo@gmail.com>
2024-12-16 16:36:12 +0800

    removed the react-router-dom implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 
 import App from './App';
-import Home from './sections/Home';
-import Contact from './sections/Contact';
-import About from './sections/About';
+// import Home from './sections/Home';
+// import Contact from './sections/Contact';
+// import About from './sections/About';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 import './index.css'
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
+    <App />
+  {/* <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
@@ -25,6 +26,6 @@ root.render(
         <Route path='about' element={<About />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter> */}
 </React.StrictMode>
 )
